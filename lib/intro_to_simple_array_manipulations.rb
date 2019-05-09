c5ce5af4c79fc996972547202a7add19c70d9b90
hao wang <ge_nah@hotmail.com>
2019-05-09 05:59:43 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(arr, string)
+  arr.push(string)
+end
+
+def using_unshift(arr, string)
+  arr.unshift(string)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr, string)
+  arr.insert(4, string)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(arr1, string)
+  arr1.delete(string)
+end
+
+def using_delete_at(arr, int)
+  arr.delete_at(int)
+end
